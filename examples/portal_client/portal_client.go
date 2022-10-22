11e33b67fb944276e6343a13f3bf73ff3e973b9e
lateralusd <xdaemonx@protonmail.ch>
2022-10-22 23:51:54 +0200

    Add portal examples

---
+++ examples/portal_client/portal_client.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/lateralusd/frida-go/frida"
+)
+
+type cmd struct {
+	Type string      `json:"type"`
+	Data interface{} `json:"data"`
+}
+
+func main() {
+	mgr := frida.NewManager()
+
+	ropts := frida.NewRemoteDeviceOptions()
+	ropts.SetOrigin("*")
+	ropts.SetToken("erhad")
+
+	dev, err := mgr.AddRemoteDevice("192.168.0.31", ropts)
+	if err != nil {
+		panic(err)
+	}
+
+	procs, err := dev.Processes()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, proc := range procs.EnumerateProcesses() {
+		fmt.Printf("[*] Process: %s => %d\n", proc.GetName(), proc.GetPid())
+	}
+
+	bus := dev.Bus()
+	bus.On("message", func(msg string) {
+		var c cmd
+		json.Unmarshal([]byte(msg), &c)
+		switch c.Type {
+		case "help":
+			fmt.Printf("\n[*] Result for \"help\"\n")
+			for k, v := range c.Data.(map[string]interface{}) {
+				fmt.Printf("%s => %s\n", k, v)
+			}
+		case "list_channels":
+			fmt.Printf("\n[*] Result for \"list_channels\"\n")
+			for _, k := range c.Data.([]interface{}) {
+				fmt.Printf("%s\n", k)
+			}
+		case "list_users":
+			fmt.Printf("\n[*] Result for \"list_users\"\n")
+			for _, k := range c.Data.([]interface{}) {
+				fmt.Printf("%s\n", k)
+			}
+		case "register":
+			fmt.Printf("\n[*] Result for \"register\"\n%s\n", c.Data)
+		case "broadcast":
+			fmt.Printf("\n[*] Server message: %s\n", c.Data)
+		case "welcome":
+			fmt.Println(c.Data)
+		case "msg":
+			mp := c.Data.(map[string]interface{})
+			fmt.Printf("\n[*] Message from \"%s\": %s\n",
+				mp["from"].(string),
+				mp["content"].(string))
+		case "channelMessage":
+			mp := c.Data.(map[string]interface{})
+			fmt.Printf("\n%s %s: %s\n",
+				mp["channel"].(string),
+				mp["from"].(string),
+				mp["content"].(string))
+		}
+	})
+	bus.Attach()
+
+	r := bufio.NewReader(os.Stdin)
+	name := "NA"
+
+	type msg struct {
+		From    string `json:"from"`
+		User    string `json:"user"`
+		Content string `json:"content"`
+	}
+
+	for {
+		fmt.Printf("command(%s)> ", name)
+		c, _, _ := r.ReadLine()
+		splitted := strings.Split(string(c), " ")
+		switch splitted[0] {
+		case "help":
+			c := cmd{
+				Type: "help",
+				Data: "",
+			}
+			d, _ := json.Marshal(c)
+			bus.Post(string(d))
+		case "list_channels":
+			c := cmd{
+				Type: "list_channels",
+				Data: "",
+			}
+			d, _ := json.Marshal(c)
+			bus.Post(string(d))
+		case "list_users":
+			c := cmd{
+				Type: "list_users",
+				Data: "",
+			}
+			d, _ := json.Marshal(c)
+			bus.Post(string(d))
+		case "register":
+			name = splitted[1]
+			c := cmd{
+				Type: "register",
+				Data: splitted[1],
+			}
+			d, _ := json.Marshal(c)
+			bus.Post(string(d))
+		case "msg":
+			c := cmd{
+				Type: "msg",
+				Data: msg{
+					From:    name,
+					User:    splitted[1],
+					Content: strings.Join(splitted[2:], " "),
+				},
+			}
+			d, _ := json.Marshal(c)
+			bus.Post(string(d))
+		case "join":
+			c := cmd{
+				Type: "join",
+				Data: splitted[1],
+			}
+			d, _ := json.Marshal(c)
+			bus.Post(string(d))
+		case "msgc":
+			c := cmd{
+				Type: "msgc",
+				Data: map[string]interface{}{
+					"from":    name,
+					"channel": splitted[1],
+					"content": strings.Join(splitted[2:], " "),
+				},
+			}
+			d, _ := json.Marshal(c)
+			bus.Post(string(d))
+		case "exit":
+			os.Exit(1)
+		}
+	}
+}
---
+++ examples/portal_server/portal_server.go
@@ -0,0 +1,231 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/lateralusd/frida-go/frida"
+)
+
+type cmd struct {
+	Type string      `json:"type"`
+	Data interface{} `json:"data"`
+}
+
+var usersConn = map[string]uint{}
+var usersList = []string{}
+var channelsUsers = map[string][]uint{}
+
+var channels = []string{
+	"#reverseengineering",
+	"#frida",
+	"#radare2",
+}
+
+var commands = map[string]string{
+	"register":      "name",
+	"list_channels": "N/A",
+	"msg":           "userName content",
+	"list_users":    "N/A",
+	"join":          "channelName",
+	"msgc":          "channelName content",
+}
+
+type msg struct {
+	From    string `json:"from"`
+	User    string `json:"user"`
+	Content string `json:"content"`
+}
+
+func main() {
+	fmt.Printf("[*] Frida version: %s\n", frida.Version())
+
+	for _, chanName := range channels {
+		channelsUsers[chanName] = []uint{}
+	}
+
+	cluster, err := frida.NewEndpointParameters(&frida.EParams{
+		Address: "0.0.0.0",
+		Port:    27052,
+		Token:   "erhad",
+		Origin:  "*",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	control, err := frida.NewEndpointParameters(&frida.EParams{
+		Address: "0.0.0.0",
+		Port:    27042,
+		Token:   "erhad",
+		Origin:  "*",
+	})
+
+	portal := frida.NewPortal(cluster, control)
+	if err := portal.Start(); err != nil {
+		panic(err)
+	}
+	defer portal.Stop()
+
+	portal.On("node_connected", func(connId uint, addr *frida.Address) {
+		fmt.Printf("[*] Node connected: %s(connId=%d)\n", addr, connId)
+	})
+
+	portal.On("node_disconnected", func(connId uint, addr *frida.Address) {
+		fmt.Printf("[*] Node disconnected: %s(connId=%d)\n", addr, connId)
+	})
+
+	portal.On("node_joined", func(connId uint, app *frida.Application) {
+		fmt.Printf("[*] Node joined: %d with app: %s\n", connId, app.Name())
+	})
+
+	portal.On("node_left", func(connId uint, app *frida.Application) {
+		fmt.Printf("[*] Node left: %d with app: %s\n", connId, app.Name())
+	})
+
+	portal.On("controller_connected", func(connId uint, addr *frida.Address) {
+		fmt.Printf("[*] Controller connected: %s(connId=%d)\n", addr, connId)
+	})
+
+	portal.On("controller_disconnected", func(connId int, addr *frida.Address) {
+		fmt.Printf("[*] Controller disconnected: %s(connId=%d)\n", addr, connId)
+	})
+
+	portal.On("authenticated", func(connId uint, sessionInfo string) {
+		fmt.Printf("[*] Authenticated: %s(connId=%d)\n", sessionInfo, connId)
+	})
+
+	portal.On("subscribe", func(connId uint) {
+		fmt.Printf("[*] Subscribed: connId=%d\n", connId)
+		portal.Post(connId, `
+		{
+			"type": "welcome", 
+			"data":"Welcome to Frida Portal"}
+		`, nil)
+	})
+
+	portal.On("message", func(connId uint, jsonData string, data []byte) {
+		var c cmd
+		json.Unmarshal([]byte(jsonData), &c)
+		switch c.Type {
+		case "help":
+			sendCommands(portal, connId)
+		case "list_channels":
+			sendChannels(portal, connId)
+		case "list_users":
+			sendUsers(portal, connId)
+		case "register":
+			registerUser(portal, connId, c.Data)
+		case "msg":
+			mp := c.Data.(map[string]interface{})
+			user := mp["user"].(string)
+			usConn, ok := usersConn[user]
+			if ok {
+				from := mp["from"]
+				content := mp["content"]
+				c := cmd{
+					Type: "msg",
+					Data: map[string]interface{}{
+						"from":    from,
+						"content": content,
+					},
+				}
+				dt, _ := json.Marshal(c)
+				portal.Post(usConn, string(dt), nil)
+			}
+		case "join":
+			joinChannel(portal, connId, c.Data)
+		case "msgc":
+			mp := c.Data.(map[string]interface{})
+			from := mp["from"]
+			chName := mp["channel"]
+			content := mp["content"]
+			if exists := channelExists(chName.(string)); exists {
+				c := cmd{
+					Type: "channelMessage",
+					Data: map[string]interface{}{
+						"from":    from,
+						"channel": chName,
+						"content": content,
+					},
+				}
+				dt, _ := json.Marshal(c)
+				portal.Narrowcast(chName.(string), string(dt), nil)
+			}
+		}
+	})
+
+	go func() {
+		t := time.NewTicker(5 * time.Minute)
+		for {
+			<-t.C
+			c := cmd{
+				Type: "broadcast",
+				Data: "There will be server shutdown at 3.00AM",
+			}
+			dt, _ := json.Marshal(c)
+			portal.Broadcast(string(dt), nil)
+		}
+	}()
+
+	r := bufio.NewReader(os.Stdin)
+	r.ReadLine()
+}
+
+func channelExists(chName string) bool {
+	for _, chn := range channels {
+		if chName == chn {
+			return true
+		}
+	}
+	return false
+}
+
+func sendCommands(portal *frida.Portal, connId uint) {
+	c := cmd{
+		Type: "help",
+		Data: commands,
+	}
+	postData(portal, connId, &c)
+}
+
+func sendChannels(portal *frida.Portal, connId uint) {
+	c := cmd{
+		Type: "list_channels",
+		Data: channels,
+	}
+	postData(portal, connId, &c)
+}
+
+func sendUsers(portal *frida.Portal, connId uint) {
+	c := cmd{
+		Type: "list_users",
+		Data: usersList,
+	}
+	postData(portal, connId, &c)
+}
+
+func registerUser(portal *frida.Portal, connId uint, data interface{}) {
+	usersConn[data.(string)] = connId
+	usersList = append(usersList, data.(string))
+	c := cmd{
+		Type: "register",
+		Data: fmt.Sprintf("Welcome %s", data.(string)),
+	}
+	postData(portal, connId, &c)
+}
+
+func joinChannel(portal *frida.Portal, connId uint, data interface{}) {
+	chName := data.(string)
+	channelsUsers[chName] = append(channelsUsers[chName], connId)
+	portal.TagConnection(connId, chName)
+}
+
+func postData(portal *frida.Portal, connId uint, c *cmd) {
+	data, _ := json.Marshal(c)
+	portal.Post(connId, string(data), nil)
+}
