a1ce23e4ca0150bba7d2af4bfa4c47d80b6b3889
lateralusd <xdaemonx@protonmail.ch>
2022-10-22 15:00:57 +0200

    Finish bus implementation

--- frida/bus.go
+++ frida/bus.go
@@ -9,18 +9,28 @@ type Bus struct {
 	bus *C.FridaBus
 }
 
-func (f *Bus) Attach() {
+func (b *Bus) On(sigName string, fn interface{}) {
+	connectClosure(unsafe.Pointer(b.bus), sigName, fn)
 }
 
-func (f *Bus) Post(data string) {
+func (b *Bus) Attach() error {
+	var err *C.GError
+	C.frida_bus_attach_sync(b.bus, nil, &err)
+	if err != nil {
+		return &FridaError{err}
+	}
+	return nil
+}
+
+func (b *Bus) Post(data string) {
 	dataC := C.CString(data)
 	defer C.free(unsafe.Pointer(dataC))
 
-	C.frida_bus_post(f.bus, dataC, nil)
+	C.frida_bus_post(b.bus, dataC, nil)
 }
 
-func (f *Bus) IsDetached() bool {
-	dt := C.int(C.frida_bus_is_detached(f.bus))
+func (b *Bus) IsDetached() bool {
+	dt := C.int(C.frida_bus_is_detached(b.bus))
 	if dt == 1 {
 		return true
 	}
